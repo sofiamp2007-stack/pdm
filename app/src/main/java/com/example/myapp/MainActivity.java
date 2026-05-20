7d7f0c2efa11ce9e0818b9a3d5530ca86c8678b1
Your Name <you@example.com>
2026-05-20 12:12:02 -0300

    banco de dados

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.myapp;
 
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,13 +16,57 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    SQLiteDatabase sqLiteDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        getString(R.string.app_name);
+
+
+        sqLiteDatabase = openOrCreateDatabase("notas", MODE_PRIVATE, null);
+
+        sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS notas (id INTEGER PRIMARY KEY AUTOINCREMENT, titulo TEXT, nota TEXT)");
+
+       // String titulo = 'João';
+        //sqLiteDatabase.execSQL("INSERT INTO notas VALUES(1,'"+titulo+"', '')");
+
+        /*nomeColuna valor */
+        ContentValues contentValues = new ContentValues();
+        contentValues.put ("titulo", "joao");
+        contentValues.put("nota", "7777777777777");
+
+        sqLiteDatabase.insert("notas ", null, contentValues);
+        ListView lv =findViewById(R.id.listView);
+
+        // RECUPERAR DADOS DO SQLITE
+        Cursor c = sqLiteDatabase.rawQuery("SELECT id, id*10,titulo, nota  FROM notas", null);
+        c.moveToFirst(); // movimenta a ponteira do cursor para o primeiro registro recuperado
+        ArrayList<Nota>listaNotas = new ArrayList<>();
+        while(!c.isAfterLast()){
+
+            @SuppressLint("Range") int id = c.getInt(c.getColumnIndex("id"));
+            @SuppressLint("Range") String titulo = c.getString(c.getColumnIndex("titulo"));
+        @SuppressLint("Range") String nota =  c.getString(c.getColumnIndex("nota"));
+        Nota n = new Nota(id,titulo,nota);
+        listaNotas.add(n);
+        Log.d("SELECT", Integer.toString(id)+", "+titulo+","+nota);
+        c.moveToNext();
+
+        ArrayList<String> listatitulos = new ArrayList<>();
+        for (Nota nota1 :listaNotas){
+            listatitulos.add(nota1.titulo);
+
         }
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, listatitulos);
+            lv.setAdapter(adapter);
+
+
+    }}
+
     }
---
+++ app/src/main/java/com/example/myapp/Nota.java
@@ -0,0 +1,17 @@
+package com.example.myapp;
+
+public class Nota {
+
+    Integer id;
+    String titulo;
+
+    String nota;
+
+    public Nota(Integer id, String titulo, String nota){
+        this.id =id;
+        this.titulo=titulo;
+        this.nota =nota;
+
+    }
+
+}
