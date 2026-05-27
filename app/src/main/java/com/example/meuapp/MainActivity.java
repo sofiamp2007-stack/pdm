63f1d9f21f9bed99f9f8ae723a01da1a965e0055
Sofia Pascoalini <Sofia.m.pascoalini@gmail.com>
2026-05-26 22:53:40 -0300

    I18

--- app/src/main/java/com/example/meuapp/MainActivity.java
+++ app/src/main/java/com/example/meuapp/MainActivity.java
@@ -1,66 +1,29 @@
 package com.example.meuapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.myapp.R;
-
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnCalcular;
-
-    EditText etPeso;
-    EditText etAltura;
-
-
-
-
+    Button b;
+    int contador = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        EdgeToEdge.enable(this);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
         EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
         getString(R.string.app_name);
-
-
-
-        etPeso = findViewById(R.id.editTextPeso);
-        etAltura = findViewById(R.id.editText);
-        btnCalcular=findViewById(R.id.buttonCalcular);
-
-
-        btnCalcular.setOnClickListener(v -> {
-
-            String pesoStr = etPeso.getText().toString();
-            String alturaStr = etAltura.getText().toString();
-
-            double peso = Double.parseDouble(pesoStr);
-            double altura = Double.parseDouble(alturaStr);
-
-            double imc = peso / (altura * altura);
-
-
-            Intent i = new Intent(getApplicationContext(), MainActivity2.class);
-
-            i.putExtra("resultado", imc);
-            startActivity(i);
-
-
-
-
+        b = findViewById(R.id.button);
+        b.setOnClickListener(v -> {
+            contador++;
+            b.setText(String.valueOf(contador));
 
         });
-
     }
-
 }
